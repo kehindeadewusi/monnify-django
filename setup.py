0a4f22e1f93be44ce4d664238cf46520c33a05b1
Kehinde Adewusi <kehindeadewusi@users.noreply.github.com>
2020-10-23 22:00:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,8 @@
+import setuptools
 
+setuptools.setup(
+  name="monnify-python",
+  version="0.1.1",
+  description="python app to make integration with monnify easier",
+  packages=setuptools.find_packages("src"),
+  package_dir={"":"src"})
